0a5e6b63f08c369f1ebba1485f8d49aa7b97979d
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-20 22:50:35 -0600

    get

--- main.go
+++ main.go
@@ -38,6 +38,10 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet"))
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Save a new snippet"))
+}
+
 func main() {
 	//router
 	/*
@@ -49,9 +53,12 @@ func main() {
 	mux := http.NewServeMux()
 
 	//catch all
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	//no overlapping
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("starting server on :4000")
 
