735b5161c272360e29fcacff4221dbbe04ecc63e
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-20 20:07:22 -0600

    routing

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from alex"))
+	w.Write([]byte("Hello from snippetBox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+
+	a := r.PathValue("id")
+
+	id, error := strconv.Atoi(a)
+
+	if error != nil || id < 1 {
+
+		http.NotFound(
+			w,
+			r)
+
+		return
+	}
+
+	msg := fmt.Sprintf(
+		"Display a specific snippet with ID %d",
+		id)
+
+	w.Write(
+		[]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
 }
 
 func main() {
@@ -19,7 +48,10 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", home)
+	//catch all
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
