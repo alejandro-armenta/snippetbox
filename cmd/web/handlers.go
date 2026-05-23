6f63433f5354aedfe260176bd83701c207916c57
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-23 16:28:42 -0600

    routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -23,9 +23,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 
 		return
 	}
@@ -34,9 +32,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 
 	}
 
