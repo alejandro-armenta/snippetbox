42390b0287aafd0cba8e6d6baa627d49bb85544b
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-26 18:31:43 -0600

    added security

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 
 	mux := http.NewServeMux()
 
@@ -14,5 +14,6 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return mux
+	//the mux is a handler
+	return commonHeaders(mux)
 }
