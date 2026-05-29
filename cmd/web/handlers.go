a1e7da5362c339a202f10b3941dcf8b0a2263252
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-29 15:49:55 -0600

    save

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -149,7 +149,7 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
 
 	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
-	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email addres")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
 
 	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
 	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
@@ -202,7 +202,53 @@ func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Authenticate and login a user")
+
+	var form userLoginForm
+
+	err := app.decodePostForm(r, &form)
+
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MaxBytes(form.Password, 72), "password", "This field must not be more than 72 bytes long")
+
+	if !form.Valid() {
+
+		data := app.newTemplateData(r)
+
+		data.Form = form
+
+		app.render(w, r, http.StatusUnprocessableEntity, "login.tmpl", data)
+
+		return
+	}
+
+	id, err := app.users.Authenticate(form.Email, form.Password)
+
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+
+			form.AddNonFieldError("Email or password is incorrect")
+
+			data := app.newTemplateData(r)
+
+			data.Form = form
+
+			app.render(w, r, http.StatusUnprocessableEntity, "login.tmpl", data)
+
+		} else {
+			app.serverError(w, r, err)
+		}
+
+		return
+	}
+
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
