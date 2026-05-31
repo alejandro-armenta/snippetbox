ab4e381d6c4e74c9494e10c4b8685b8b47ab2cba
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-30 22:42:01 -0600

    made build

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/justinas/alice"
+	"snippetbox.alexarmenta.net/ui"
 )
 
 //cross origin is stricter than cross site
@@ -12,8 +13,7 @@ func (app *application) routes() http.Handler {
 
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, preventCSRF, app.authenticate)
 
