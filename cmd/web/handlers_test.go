525a42aab9f2f9bd269180e8ca40bef279e14c39
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-31 22:52:37 -0600

    save

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"snippetbox.alexarmenta.net/internal/assert"
+)
+
+func TestPing(t *testing.T) {
+
+	rr := httptest.NewRecorder()
+
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ping(rr, req)
+
+	res := rr.Result()
+
+	defer res.Body.Close()
+
+	assert.Equal(t, res.StatusCode, http.StatusOK)
+
+	body, err := io.ReadAll(res.Body)
+
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+
+}
